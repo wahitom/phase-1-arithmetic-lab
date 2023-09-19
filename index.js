9e0c1a0bc454120a13189264903675fe244f3a62
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-19 12:06:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+
+const random =  Math.round( Math.random() * 10) + 1;
+console.log(Number.isInteger(random));
+console.log(random);
+
+const num3 = 4;
+const num4 = 1;
+const mod = num3 / num4; 
+console.log(mod)
+
+const max = Math.max(10, 15, 17, 20);
+console.log(max);
